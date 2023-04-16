94e6d44de4c7ec6f121748c26554edb64bfbcdf9
Nguyen Truong Bach <bachnguyentruong2003@gmail.com>
2023-04-16 17:47:58 +0700

    update

--- src/LayoutComponent/BTthuchanhLayOut.jsx
+++ src/LayoutComponent/BTthuchanhLayOut.jsx
@@ -4,7 +4,7 @@ import Body from './Body'
 import Footer from './Footer'
 import './style.css';
 
-export default class BaiTapThucHanhLayOut extends Component {
+export default class BTthuchanhLayOut extends Component {
   render() {
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import BaiTapThucHanhLayOut from './BaiTapLayoutComponent/BaiTapThucHanhLayOut';
+import BTthuchanhLayOut from './LayoutComponent/BTthuchanhLayOut';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // <BaiTapThucHanhLayOut/>
     <div>
-      <BaiTapThucHanhLayOut/>
+      <BTthuchanhLayOut/>
     </div>
     
 );
